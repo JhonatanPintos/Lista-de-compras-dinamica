924fd18ace1cec137a858d37a6f29f919016d3db
jhonatan <pintos108@gmail.com>
2022-07-14 21:29:43 -0300

    nuevas correciones y modificaciones, se elimino 2 else

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ class Productos {
         this.cantidad = cantidad
     }
 }
+//La idea es que la lista este vacia y el usuario cargue cada producto, puse estos po default para que sea mas facil testear las cosas
 let listaProd = [{
         nombre: "televisor",
         precio: 1000,
@@ -30,17 +31,12 @@ let listaProd = [{
         precio: 1500,
         cantidad: 1
     },
-    {
-        nombre: "computadora",
-        precio: 3000,
-        cantidad: 1
-    },
 ]
 let opciones
 do {
-    opciones = parseInt(prompt("Que de sea hacer?\n Elija una opcion \n 1.Agregar Producto \n 2.Modificar o eliminar un producto \n 3.Mostrar Lista \n 4.Salir de la aplicacion"))
+    opciones = parseInt(prompt("Que de sea hacer?\n Elija una opcion \n 1.Agregar Producto \n 2.Modificar o eliminar un producto \n 3.Mostrar y totalizar Lista \n 4.Salir de la aplicacion"))
     if (opciones == 1) {
-        //Opcion 1 es para agregar el nombre, preio y cantidad de los productos
+        //Opcion 1 - es para agregar el nombre, preio y cantidad de los productos
         let opciones1
         do {
             opciones1 = parseInt(prompt("Elija una opcion \n 1.Agregar Producto \n 2.Volver al inicio"))
@@ -77,11 +73,13 @@ do {
         } while (opciones1 != "2")
     }
     if (opciones == 2) {
-        //Opcion 2 es para modificar o eliminar un producto de la lista
+        //Opcion 2 - es para modificar o eliminar un producto de la lista
         let opciones2
         do {
+            //Aca se define si se "elimina" o "modifica" el producto que el usuariuo desee
             opciones2 = parseInt(prompt("Elija una opcion \n 1.Eliminar Producto \n 2.Modificar Producto \n 3.Volver al inicio"))
             if (opciones2 == 1) {
+                //En este caso/opcion se elimina el producto
                 alert(`Cual de estos productos desea eliminar?\n ${JSON.stringify(listaProd)}`)
                 let eliminar = prompt("Ingrese el nombre del producto desee eliminar")
                 listaProd.forEach((prod, index) => {
@@ -89,12 +87,11 @@ do {
                         listaProd.splice(index, 1)
                         alert(`El producto "${eliminar}" fue eliminado con exito`)
                         alert(`Su lista quedo conformada de esta manera\n ${JSON.stringify(listaProd)}`)
-                    } else {
-                    alert("Buscando...")
                     }
                 })
             }
             if(opciones2 == 2){
+                //En este caso/opcion se modifica un producto eliminandolo e instantanemanete cargando uno nuevo
                 alert(`Cual de estos productos desea modificar?\n ${JSON.stringify(listaProd)}`)
                 let modificar = prompt("Ingrese el nombre del producto desee modificar")
                 listaProd.forEach((prod2, index2) => {
@@ -107,14 +104,13 @@ do {
                         listaProd.push(prod2)
                         alert(`El producto "${modificar}" fue modificado con exito`)
                         alert(`Su lista quedo conformada de esta manera\n ${JSON.stringify(listaProd)}`)
-                    } else {
-                        alert("No se encontro ningun producto con ese nombre")
                     }
                 })
             }
         } while (opciones2 != "3")
     }
     if (opciones == 3) {
+        //Opcion 3 - En este caso/opcion se muestra y totaliza la lista completa, mostrando asi sus nombres, precios y cantidades los cuales son totalizados individualmente y luego se totalizan todos los producto que tenga la lista
         let acumulador = 0
         listaProd.forEach(producto => {
             acumulador += (producto.precio * producto.cantidad)
@@ -123,4 +119,5 @@ do {
         console.log(`La compra total de sus productos tiene un total de "${acumulador}$"`)
     }
 } while (opciones != "4")
+//Opcion 4 - se sale de la aplicacion
 alert("gracias por utilizar nuestra aplicacion!")
