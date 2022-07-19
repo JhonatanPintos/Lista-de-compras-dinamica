c61692b91c31a8f60c8b5b89edca208eea14e64f
jhonatan <pintos108@gmail.com>
2022-07-19 18:58:06 -0300

    nuevas correciones y funcion mostrar

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ let listaProd = [{
 
 let agregar = document.querySelector("#agregar")
 let eliminarProd = document.querySelector("#eliminar")
+let mostrar = document.querySelector("#mostrar")
 let totalizar = document.querySelector("#total")
 
 agregar.onclick = () => {
@@ -42,6 +43,9 @@ agregar.onclick = () => {
 }
 eliminar.onclick = () => {
     eliminarProducto()
+}
+mostrar.onclick = () => {
+    mostrarLista()
 }
 totalizar.onclick = () => {
     totalProds()
@@ -52,10 +56,14 @@ const agregarProducto = () => {
     let nombre = document.querySelector("#nombre").value
     let precio = parseFloat(document.querySelector("#precio").value)
     let cantidad = parseFloat(document.querySelector("#cantidad").value)
+if((nombre != "") && (precio == isNaN()) && (cantidad == isNaN())){
     let prodNuevo = new Productos(nombre, precio, cantidad)
     listaProd.push(prodNuevo)
     escribirLista()
     return prodNuevo
+}else{
+    alert("ERROR! no ingreso dato")
+}
 }
 const escribirLista = () => {
     //En esta seccion se agregan y muestran los productos dentro de la lista en el HTML
@@ -78,12 +86,29 @@ const eliminarProducto = () => {
             listaProd.splice(index, 1)
             escribirLista()
             return
+        }else{
+            alert("No se encontro producto con ese nombre")
         }
+    })
+}
+
+const mostrarLista = () => {
+    //En esta seccion solo se imprimira la lista en el HTML
+    document.getElementById("listaProd").innerHTML = ""
+    listaProd.forEach(producto => {
+        let nodo = document.createElement("div")
+        nodo.className = "prods"
+        nodo.setAttribute("id", producto.nombre)
+        nodo.innerHTML = `<p>${producto.nombre}</p>
+                        <p>${producto.precio}</p>
+                        <p>${producto.cantidad}</p>`
+        document.getElementById("listaProd").appendChild(nodo)
     })
 }
 
 const totalProds = () => {
     //En esta seccion se totalizara el total de los productos multiplicando sus precios con sus cantidades
+    document.getElementById("precioTotal").innerHTML = "Total de su compra"
     let acumulador = 0
     listaProd.forEach(producto => {
         acumulador += (producto.precio * producto.cantidad)
